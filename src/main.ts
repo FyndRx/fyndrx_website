da985114324286eea70022663bf71512910a8913
abytrone <abdulbasit.yahaya51@gmail.com>
2026-02-10 21:32:03 +0000

    feat: add sentry integration

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import * as Sentry from "@sentry/vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css";
@@ -9,5 +10,19 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+Sentry.init({
+  app,
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when an error occurs.
+});
 
 app.mount("#app");
