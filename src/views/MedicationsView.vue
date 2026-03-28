54c987910d9a4300d249cf22dfb7eeb316d01e6e
abytrone <abdulbasit.yahaya51@gmail.com>
2026-03-28 12:29:30 +0000

    Reverse image URL prepending; rely on API for full URLs

--- src/views/MedicationsView.vue
+++ src/views/MedicationsView.vue
@@ -15,6 +15,7 @@ import MedicationQuickViewModal from '@/components/MedicationQuickViewModal.vue'
 import MedicationComparison from '@/components/MedicationComparison.vue';
 import FavoriteButton from '@/components/FavoriteButton.vue';
 import Pagination from '@/components/Pagination.vue';
+import { getImageUrl } from '@/utils/imageUrl';
 
 useSeoMeta({
   title: 'Browse Medications | FyndRx',
